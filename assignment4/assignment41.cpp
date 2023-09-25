f856f9dfe2aefb8e5e293407f4d4db47db4208a0
NirajShaha <nirajshahatakri@gmail.com>
2023-09-25 18:50:07 +0530

    assignment Que1 and Que2 added

---
+++ assignment4/assignment41.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+class Time{
+
+    private:
+        int hr,min,sec;
+    public:
+        Time(){
+            this->hr = 0;
+            this->min = 0;
+            this->sec = 0;
+        }
+        Time(int hr,int min,int sec){
+            this->hr = hr;
+            this->min = min;
+            this->sec = sec;
+        }
+        void acceptTime(){
+            cout<<"Enter hour: ";
+            cin>>this->hr;
+            cout<<"Enter min: ";
+            cin>>this->min;
+            cout<<"Enter sec: ";
+            cin>>this->sec;
+        }
+        void displayTime(){
+            cout<<"Hour: "<<this->hr<<endl;
+            cout<<"Min: "<<this->min<<endl;
+            cout<<"Sec: "<<this->sec<<endl;
+        }
+        void getHour(){
+            cout<<"Current Hour: "<<this->hr<<endl;
+        }
+        void getMin(){
+            cout<<"Current Min: "<<this->min<<endl;
+        }
+        void getSec(){
+            cout<<"Current Min: "<<this->sec<<endl;
+        }
+        void printTime(){
+            cout<<"current time is: "<<this->hr<<":"<<this->min<<":"<<this->sec<<endl;
+        }
+
+        void setHr(){
+            cout<<"Change Hour: "<<endl;
+            cin>>hr;
+        }
+        void setMin(){
+            cout<<"Change Min: "<<endl;
+            cin>>min;
+        }
+        void setSec(){
+            cout<<"Change Seconds: "<<endl;
+            cin>>sec;
+        }
+};
+
+int main(){
+    int size;
+    cout<<"Enter size: ";
+    cin>>size;
+    Time **arr = new Time*[size];
+    //on stack loc    //on heap loc
+    //dynamic memory allocation for objects
+    for(int i = 0;i<size;i++){
+        arr[i] = new Time();
+    }
+
+    //keeping values in the array of objects
+    for(int i = 0 ; i < size ; i++){
+        cout<<"Enter values for index "<< i << " = " <<endl;
+        arr[i] -> acceptTime();
+    }
+    //displaying the objects
+    for(int i = 0 ; i < size ; i++){
+        cout<<"values at index:  "<< i << " = " <<endl;
+        //cout<<"value add of arr[" << i <<"] is: "<<&*arr[i]<<endl;
+        arr[i] -> displayTime();
+    }
+    //for deallocating objects memory
+    for(int i = 0 ; i < size ; i++){
+        delete arr[i];
+    }
+    delete[] arr;
+
+    //array of objects
+    /*Time arr[5];
+
+    for(int i = 0 ; i < 5 ; i++){
+        cout<<"Enter the values of Time,min,sec for index "<<i<<" = "<<endl;
+        arr[i].acceptTime();
+    }
+
+    for(int i = 0; i<5;i++){
+        cout<<"values at index "<< i << " = " <<endl;
+        arr[i].displayTime();
+    }*/
+    return 0;
+}
---
+++ assignment4/assignment42.cpp
@@ -0,0 +1,145 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+#define SIZE 5
+
+class Stack{
+    private:
+        int a;
+        int top;
+        int size;
+        int *arr; //array of pointers
+    public:
+        Stack(int sz){
+            this->top = -1;
+            if(size!=0){
+                this->arr = new int[sz];
+                this->size = sz;
+            }else{
+                this->arr = new int[SIZE];
+                this->size = SIZE;
+            }
+        }
+        void push(){
+            if(this->top == size -1){
+                cout<<"Stack is full"<<endl;
+            }
+            else{
+                this->top++;
+                cout<<"Enter a number: ";
+                cin>>a;
+                this->arr[top] = a;
+            }
+        }
+        void pop(){
+            if(this->top == -1){
+                cout<<"stack is empty"<<endl;
+            }
+            else{
+                this->top--;
+            }
+
+        }
+        void peek(){
+            if(this->top == -1){
+                cout<<"Stack is empty"<<endl;
+            }else{
+                cout<<"peeked value is: "<<this->arr[top]<<endl;
+            }
+        }
+        void isEmpty(){
+            if(this->top == -1){
+                cout<<"Stack is empty"<<endl;
+            }else{
+                cout<<"stack is not empty"<<endl;
+            }
+        }
+        void isFull(){
+            if(this->top == size-1){
+                cout<<"Stack is full"<<endl;
+            }else{
+                cout<<"stack is not full"<<endl;
+            }
+        }
+        void print(){
+            if(this->top == -1 or this->top == size-1){
+                cout<<"not possible"<<endl;
+            }else{
+                for (int i = 0 ; i < size ; i++){
+                    cout<<this->arr[i]<<endl;
+                }
+            }
+        }
+};
+
+enum Emenu{
+    EXIT,
+    PUSH,
+    POP,
+    PEEK,
+    ISEMPTY,
+    ISFULL,
+    PRINT
+};
+
+Emenu menu(){
+    int choice;
+    cout<<"**********************************"<<endl;
+    cout<<"0. EXIT."<<endl;
+    cout<<"1. PUSH."<<endl;
+    cout<<"2. POP."<<endl;
+    cout<<"3. PEEK."<<endl;
+    cout<<"4. ISEMPTY."<<endl;
+    cout<<"5. ISFULL."<<endl;
+    cout<<"6. PRINT."<<endl;
+    cout<<"Enter your choice: "<<endl;
+    cin>>choice;
+    cout<<"***********************************"<<endl;
+    return Emenu(choice);
+}
+
+int main(){
+    int size;
+    cout<<"Enter size: ";
+    cin>>size;
+    /*Stack s(6);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    s.pop();
+    s.print();*/
+
+    class Stack stack(size);
+    Emenu choice;
+    while((choice=menu())!=EXIT){
+        switch(choice){
+            case EXIT:
+                cout<<"exiting........."<<endl;
+                break;
+            case PUSH:
+                stack.push();
+                break;
+            case POP:
+                stack.pop();
+                break;
+            case PEEK:
+                stack.peek();
+                break;
+            case ISEMPTY:
+                stack.isEmpty();
+                break;
+            case ISFULL:
+                stack.isFull();
+                break;
+            case PRINT:
+                stack.print();
+                break;
+        }
+    }
+    cout<<"thank you for choosing"<<endl;
+    return 0;
+}
